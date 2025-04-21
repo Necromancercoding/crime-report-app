ca56ed1c8e3c2a4163e9b243f45729f445faec9c
Necromancercoding <69889146+Necromancercoding@users.noreply.github.com>
2025-04-21 14:58:48 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,10 @@
+// models/User.js
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: {
+  name: {
     type: String,
     required: true,
-    unique: true,
   },
   email: {
     type: String,
