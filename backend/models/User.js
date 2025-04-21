a6fe9c12b7934355c6257cb787ece89d266429c3
Necromancercoding <69889146+Necromancercoding@users.noreply.github.com>
2025-04-21 14:54:58 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
 
-// Define the user schema
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-// Create a model for the schema
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
